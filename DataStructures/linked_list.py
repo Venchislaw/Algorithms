e6e3fe03ca1b0ad3ff3611b00bb908f45f399d06
Venchislaw <venchislavcodes@gmail.com>
2024-10-29 19:02:32 +0600

    I'm chippin' in🤘
    That's what happens to the person playing CyberPunk2077...
    Btw I don't give a f that I didn't finish code
    I'll do it later🤘

---
+++ DataStructures/linked_list.py
@@ -0,0 +1,58 @@
+class SinglyListNode:
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+    
+    def __repr__(self):
+        return f"Node({self.val} | next={self.next})"
+
+
+class DoublyListNode:
+    def __init__(self, val, prev=None, next=None):
+        self.val = val
+        self.prev = prev
+        self.next = next
+
+
+class SinglyLinkedList:
+    def __init__(self, head=None):
+        self.head = head
+        self.tail = head
+    
+    def __repr__(self):
+        cur = self.head
+        res = []
+        while cur:
+            res.append(str(cur.val))
+            cur = cur.next
+        return ", ".join(res)
+        
+    def insert(self, val, strategy="head") -> None:
+        if strategy == "head":
+            node = SinglyListNode(val)
+            node.next = self.head
+            self.head = node
+            if not self.head.next:
+                self.tail = self.head
+        elif strategy == "tail":
+            node = SinglyListNode(val)
+            if not self.head:
+                self.head = node
+                self.tail = node
+            self.tail.next = node
+            self.tail = node
+    
+
+list_ = SinglyLinkedList()
+list_.insert(1)
+list_.insert(2)
+list_.insert(3)
+print(list_)  # 3, 2, 1
+print(list_.head, list_.tail)
+print("-" * 60)
+list_ = SinglyLinkedList()
+list_.insert(1, strategy="tail")
+list_.insert(2, strategy="tail")
+list_.insert(3, strategy="tail")
+print(list_)  # 1, 2, 3
+print(list_.head, list_.tail)
